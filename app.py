d873042dc363103d9ba2a790e97e9c3846f43403
icecocoasince2003-create <icecocoa.since2003@gmail.com>
2026-04-01 17:54:23 +0900

    seventh commit

--- app.py
+++ app.py
@@ -5,24 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", page="index")
+    return render_template("index.html")
 
 @app.route("/resume")
 def resume():
-    return render_template(
-        "resume.html",
-        page="resume",
-        qualifications=get_qualifications()
-    )
+    qualifications = get_qualifications()
+    return render_template("resume.html", qualifications=qualifications)
 
 @app.route("/activity")
 def activity():
-    return render_template(
-        "activity.html",
-        page="activity",
-        achievements=get_achievements(),
-        societies=get_societies()
-    )
+    achievements = get_achievements()
+    societies = get_societies()
+    return render_template("activity.html", achievements=achievements, societies=societies)
 
 if __name__ == "__main__":
     app.run(debug=True)
