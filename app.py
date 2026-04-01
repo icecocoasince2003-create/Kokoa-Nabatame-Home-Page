cebf998cf3d10273eb72366512e33b0a3af1314f
icecocoasince2003-create <icecocoa.since2003@gmail.com>
2026-04-01 17:07:32 +0900

    fourth commit

--- app.py
+++ app.py
@@ -3,26 +3,26 @@ from data.loader import get_achievements, get_societies, get_qualifications
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html', page='index')
+    return render_template("index.html", page="index")
 
-@app.route('/resume')
+@app.route("/resume")
 def resume():
     return render_template(
-        'resume.html',
-        page='resume',
+        "resume.html",
+        page="resume",
         qualifications=get_qualifications()
     )
 
-@app.route('/activity')
+@app.route("/activity")
 def activity():
     return render_template(
-        'activity.html',
-        page='activity',
+        "activity.html",
+        page="activity",
         achievements=get_achievements(),
         societies=get_societies()
     )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
